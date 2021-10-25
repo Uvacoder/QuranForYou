82acee7d11b8ca3e55f22cad3792361ede78be5b
Divyansh Prateek <divyansh@mobiux.in>
2021-10-25 11:09:06 +0530

    add: setup axios and interceptors

--- quran-for-you/src/main.js
+++ quran-for-you/src/main.js
@@ -2,8 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueMq from 'vue-mq'
+import VueMq from "vue-mq";
 import customTheme from "./custom-theme";
+import axios from "./axios";
 
 import Chakra, {
   CThemeProvider,
@@ -12,9 +13,8 @@ import Chakra, {
   CReset,
 } from "@chakra-ui/vue";
 
-
 Vue.use(Chakra, {
-  extendTheme: customTheme
+  extendTheme: customTheme,
 });
 
 Vue.use(VueMq, {
@@ -23,10 +23,67 @@ Vue.use(VueMq, {
     tablet: 900,
     laptop: 1250,
     desktop: Infinity,
-  }
-})
+  },
+});
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios;
+
+axios.defaults.timeout = 10000;
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.common["Authorization"] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  (response) => {
+    if (response.status === 200 || response.status === 201) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  (error) => {
+    if (error.response.status) {
+      switch (error.response.status) {
+        case 400:
+          // handle 400 error
+          break;
+
+        case 401:
+          // session expired
+          break;
+        case 403:
+          router.replace({
+            path: "/login",
+            query: { redirect: router.currentRoute.fullPath },
+          });
+          break;
+        case 404:
+          // handle 404
+          break;
+        case 502:
+          setTimeout(() => {
+            router.replace({
+              path: "/login",
+              query: {
+                redirect: router.currentRoute.fullPath,
+              },
+            });
+          }, 1000);
+      }
+      return Promise.reject(error.response);
+    }
+  }
+);
 
 new Vue({
   router,
