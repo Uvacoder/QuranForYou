0d6ca9584d8dd95ecda75f365118859f1aa7b276
Bhavya Babuta <babutabhavya@gmail.com>
2021-10-28 16:45:02 +0530

    Change chapters in verse

---
+++ quran-for-you/src/constants/index.js
@@ -0,0 +1,19 @@
+export const FONT_SIZES = [
+    { value: "16px", font: 16 },
+    { value: "18px", font: 18 },
+    { value: "20px", font: 20 },
+    { value: "22px", font: 22 },
+    { value: "24px", font: 24 },
+    { value: "26px", font: 26 },
+];
+
+export const MAIN_STYLES = {
+    dark: {
+        bg: "black",
+        color: "whiteAlpha.900",
+    },
+    light: {
+        bg: "white",
+        color: "gray.900",
+    },
+};
